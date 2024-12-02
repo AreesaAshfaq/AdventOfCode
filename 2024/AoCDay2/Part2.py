d2e6b65d62137cf530021f341e0daa3d26cec1a9
Areesa Ashfaq <85050863+AreesaAshfaq@users.noreply.github.com>
2024-12-02 14:36:11 +0500

    Create Part2.py

---
+++ 2024/AoCDay2/Part2.py
@@ -0,0 +1,32 @@
+d2 = open("/2024_Day2_input.txt", "r")
+totalSafeReports = 0
+
+def isIncrease(level):
+  for l in range(len(level)-1):
+    diff = abs(level[l] - level[l+1])
+    if level[l] < level[l+1] and diff >= 1 and diff <=3:
+      continue
+    else:
+      return False
+  return True
+def isDecrease(level):
+  for l in range(len(level)-1):
+    diff = abs(level[l] - level[l+1])
+    if level[l] > level[l+1] and diff >= 1 and diff <=3:
+      continue
+    else:
+      return False
+  return True
+def isRemovingOne(level):
+  for l in range(len(level)):
+    newLevel = level[:l] + level[l+1:]
+    if isIncrease(newLevel) or isDecrease(newLevel):
+      return True
+  return False
+
+for level in d2:
+  curLevel = list(map(int, level.strip().split())) 
+  #print(isIncrease(curLevel), isDecrease(curLevel), isRemovingOne(curLevel))
+  if isRemovingOne(curLevel):
+    totalSafeReports += 1
+print(totalSafeReports)
